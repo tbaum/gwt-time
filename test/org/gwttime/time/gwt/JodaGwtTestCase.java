48b2b2b4c7ff2526e232f76901b3ab6893a9c8b0
scottfines <scottfines@d9327d18-20e4-11df-a502-17d33bbe96c3>
2010-02-24 03:36:17 +0000

    refactoring into new package names. Ready to begin modifications
    git-svn-id: http://gwt-time.googlecode.com/svn/gwt-time/trunk@19 d9327d18-20e4-11df-a502-17d33bbe96c3

--- test/org/gwttime/time/gwt/JodaGwtTestCase.java
+++ test/org/gwttime/time/gwt/JodaGwtTestCase.java
@@ -24,7 +24,7 @@ public class JodaGwtTestCase extends GWTTestCase {
 
     @Override
     public String getModuleName() {
-        return "org.joda.TimeTest";
+        return "org.gwttime.TimeTest";
     }
 
 }
